b49db564e785e20dc264479173c184265e1ff686
Gabriel Santos <cl.gabrielzsantos@gmail.com>
2023-09-06 15:33:08 -0300

    Spring Data JPA

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,12 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DaadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,8 @@ public class MedicoController {
     @Autowired
     private MedicoRepository repository;
     @PostMapping
-    public void cadastrar(@RequestBody DaadosCadastroMedico dados) {
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DaadosCadastroMedico dados) {
 
         repository.save(new Medico(dados));
 
